a8b142973239b1ecae07aa07fd2e59d5389652a7
lwd-temp <26019675+lwd-temp@users.noreply.github.com>
2018-09-30 18:10:43 +0800

    Add files via upload

---
+++ happybirthdayzwt.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+# coding=utf-8
+def main():
+    from datetime import datetime
+    date=datetime.today()
+    if str(date.month)=="4":
+        if int(date.day)>=20:
+            print("Happy birthday ZWT!")
+    if str(date.month)=="5":
+        if str(date.day)=="5":
+            count=0
+            while count!=4:
+                print("Happy birthday ZWT!")
+                count=count+1
+        if int(date.day)<=15:
+            print("Happy birthday ZWT!")
