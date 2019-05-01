219dbc2a9bde942e0d1d457c864bf6fbd5ba068f
lwd-temp <26019675+lwd-temp@users.noreply.github.com>
2019-05-01 22:08:13 +0800

    Update happybirthdayzwt.py

--- happybirthdayzwt.py
+++ happybirthdayzwt.py
@@ -4,16 +4,16 @@
 # Author:LWD
 # This code is used as an Easter Egg.
 def main():
-    from datetime import datetime
-    date=datetime.today()
-    if str(date.month)=="4":
-        if int(date.day)>=20:
+    # For the most adorable one.
+    date=datetime.datetime.today()
+    if date.month==4:
+        if date.day>=20:
             print("Happy birthday ZWT!")
-    if str(date.month)=="5":
-        if str(date.day)=="5":
+    if date.month==5:
+        if date.day==5:
             count=0
             while count!=4:
                 print("Happy birthday ZWT!")
                 count=count+1
-        if int(date.day)<=15:
+        if date.day<=15:
             print("Happy birthday ZWT!")
